ea9066ac3cebaedc2239e541c644c273bbd84bd6
Zuellni <123005779+Zuellni@users.noreply.github.com>
2024-07-29 17:04:04 +0200

    Read settings from config file

--- translate_vision.py
+++ translate_vision.py
@@ -1,3 +1,4 @@
+import json
 from argparse import ArgumentParser
 from pathlib import Path
 from warnings import filterwarnings
@@ -32,20 +33,6 @@ from rich.progress import (
 )
 from transformers import AutoProcessor, AutoModelForCausalLM
 
-parser = ArgumentParser()
-parser.add_argument("-a", "--captioning_model", type=Path, required=True)
-parser.add_argument("-m", "--translation_model", type=Path, required=True)
-parser.add_argument("-i", "--input", type=Path, required=True)
-parser.add_argument("-v", "--video", type=Path, required=True)
-parser.add_argument("-d", "--device", type=str, default="cuda")
-parser.add_argument("-f", "--lang_from", type=str, default="")
-parser.add_argument("-t", "--lang_to", type=str, default="")
-parser.add_argument("-b", "--batch", type=int, default=8)
-parser.add_argument("-c", "--cache_bits", type=int, default=16, choices=(4, 6, 8, 16))
-parser.add_argument("-l", "--line_len", type=int, default=128)
-parser.add_argument("-s", "--seq_len", type=int, default=2048)
-args = parser.parse_args()
-
 progress = Progress(
     TextColumn("{task.description}"),
     BarColumn(),
@@ -56,6 +43,15 @@ progress = Progress(
     TimeRemainingColumn(),
 )
 
+parser = ArgumentParser()
+parser.add_argument("-c", "--config", type=Path, default="config.json")
+parser.add_argument("-i", "--input", type=Path, required=True)
+parser.add_argument("-v", "--video", type=Path, required=True)
+parser.add_argument("-f", "--lang_from", type=str, default="")
+parser.add_argument("-t", "--lang_to", type=str, default="")
+args = parser.parse_args()
+
+config = json.loads(args.config.read_text())
 lang_from = args.lang_from.capitalize().strip()
 lang_to = args.lang_to.capitalize().strip()
 lang_code = pycountry.languages.get(name=lang_to) if lang_to else ""
@@ -86,7 +82,7 @@ with progress as p, torch.inference_mode():
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         image = Image.fromarray(frame)
 
-        if not images or len(images[-1]) == args.batch:
+        if not images or len(images[-1]) == config["captioning"]["batch_size"]:
             images.append([])
 
         images[-1].append(image)
@@ -97,17 +93,17 @@ with progress as p, torch.inference_mode():
     loading = p.add_task("Loading captioning model", total=3)
 
     captioner = AutoModelForCausalLM.from_pretrained(
-        pretrained_model_name_or_path=args.captioning_model,
+        pretrained_model_name_or_path=config["captioning"]["model"],
         trust_remote_code=True,
     )
 
     p.advance(loading)
 
-    captioner.to(device=args.device, dtype=torch.bfloat16).eval()
+    captioner.to(device=config["captioning"]["device"], dtype=torch.bfloat16).eval()
     p.advance(loading)
 
     processor = AutoProcessor.from_pretrained(
-        pretrained_model_name_or_path=args.captioning_model,
+        pretrained_model_name_or_path=config["captioning"]["model"],
         trust_remote_code=True,
     )
 
@@ -118,13 +114,13 @@ with progress as p, torch.inference_mode():
     captions = []
 
     for batch in images:
-        ids = processor(text=[task] * len(batch), images=batch, return_tensors="pt")
-        ids.to(device=args.device, dtype=torch.bfloat16)
+        inputs = processor(text=[task] * len(batch), images=batch, return_tensors="pt")
+        inputs.to(device=config["captioning"]["device"], dtype=torch.bfloat16)
 
         outputs = captioner.generate(
-            input_ids=ids["input_ids"],
-            pixel_values=ids["pixel_values"],
-            max_new_tokens=args.line_len,
+            input_ids=inputs["input_ids"],
+            pixel_values=inputs["pixel_values"],
+            max_new_tokens=config["captioning"]["max_new_tokens"],
         )
 
         outputs = processor.batch_decode(outputs, skip_special_tokens=True)
@@ -134,16 +130,16 @@ with progress as p, torch.inference_mode():
     del captioner, processor
     torch.cuda.empty_cache()
 
-    config = ExLlamaV2Config(str(args.translation_model))
-    config.max_seq_len = args.seq_len
-    config.fasttensors = True
+    model_config = ExLlamaV2Config(config["translation"]["model"])
+    model_config.max_seq_len = config["translation"]["max_seq_len"]
+    model_config.fasttensors = True
 
-    tokenizer = ExLlamaV2Tokenizer(config)
+    tokenizer = ExLlamaV2Tokenizer(model_config)
     template = tokenizer.tokenizer_config_dict["chat_template"]
     template = Template(template)
 
-    settings = ExLlamaV2Sampler.Settings()
-    settings.greedy()
+    gen_settings = ExLlamaV2Sampler.Settings()
+    gen_settings.greedy()
 
     caches = {
         4: lambda m: ExLlamaV2Cache_Q4(m, lazy=True),
@@ -152,14 +148,14 @@ with progress as p, torch.inference_mode():
         16: lambda m: ExLlamaV2Cache(m, lazy=True),
     }
 
-    model = ExLlamaV2(config)
-    cache = caches[args.cache_bits](model)
+    model = ExLlamaV2(model_config)
+    cache = caches[config["translation"]["cache_bits"]](model)
 
     loading = p.add_task("Loading translation model", total=len(model.modules) + 1)
     model.load_autosplit(cache, callback=lambda _, __: p.advance(loading))
 
     generator = ExLlamaV2DynamicGenerator(model, cache, tokenizer)
-    stop = [tokenizer.eos_token_id, "\n"]
+    stop_conditions = [tokenizer.eos_token_id, "\n"]
     output = ""
 
 for index, line in enumerate(lines):
@@ -169,27 +165,43 @@ for index, line in enumerate(lines):
     caption = captions[index].strip()
     line = line.text.strip()
 
-    instruction = (
+    system = (
         f"Translate the following caption{from_lang_from}{to_lang_to}. "
         "Use the provided screenshot description and previously translated captions, "
         "if available and relevant, as context for the current translation. "
-        "Respond with the translation only, without adding anything.\n"
+        "Respond with the translation only, without adding anything."
+    )
+
+    user = (
         f"Screenshot description: {caption}\n{previous}"
         f"Caption: {line}"
     )
 
-    prompt = template.render(
-        messages=[{"role": "user", "content": instruction}],
-        bos_token=tokenizer.bos_token,
-        add_generation_prompt=True,
-    )
+    try:
+        prompt = template.render(
+            messages=[
+                {"role": "system", "content": system},
+                {"role": "user", "content": user}
+            ],
+            bos_token=tokenizer.bos_token,
+            add_generation_prompt=True,
+        )
+    except:
+        prompt = template.render(
+            messages=[
+                {"role": "user", "content": f"{system}\n{user}"}
+            ],
+            bos_token=tokenizer.bos_token,
+            add_generation_prompt=True,
+        )
 
     output = generator.generate(
         prompt=prompt,
-        max_new_tokens=args.line_len,
-        gen_settings=settings,
+        max_new_tokens=config["translation"]["max_new_tokens"],
+        gen_settings=gen_settings,
         encode_special_tokens=True,
-        stop_conditions=stop,
+        stop_conditions=stop_conditions,
+        add_bos=True,
         completion_only=True,
     ).strip()
 
